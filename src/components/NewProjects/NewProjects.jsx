8f148a04bc54edc6e8efc8c97306da0d738f53cc
Kelvin <ctsang94@gmail.com>
2024-07-02 17:41:29 -0400

    Add hover effect on main page

--- src/components/NewProjects/NewProjects.jsx
+++ src/components/NewProjects/NewProjects.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import "./NewProjects.scss";
 import homeProjects from "../../data/homeRenovationProjects.json";
+import basement from '../../assets/images/basement.jpg'
+import bathroom from '../../assets/images/bathroom.jpg'
+import bedroom from '../../assets/images/bedroom.jpg'
+import kitchen from '../../assets/images/kitchen.jpg'
+import livingroom from '../../assets/images/livingroom.jpg'
+import HoverCarousel from 'hover-carousel'
 
 function NewProjects() {
+
+  const images= [{'image': basement, 'project': 'Basement Remodeling'},{'image': bathroom, 'project': 'Bathroom Remodeling'} , {'image' :  bedroom, 'project': 'Bedroom Remodeling' }, {'image': kitchen , 'project': 'Kitchen Remodeling' }, {'image': livingroom, 'project': 'Living Room Remodeling'} ];
   return (
     <main className="new-projects">
       <h2 className="new-projects__title">Get started on your next project</h2>
       <section className="new-projects__list">
-        {homeProjects.map((item) => (
+        {images.map((item) => (
           <article className="new-projects__item">
             <img
               className="new-projects__item-image"
-              src={item.image_url}
+              src={item.image}
             ></img>
             <h3 className="new-projects__item-title">{item.project}</h3>
           </article>
