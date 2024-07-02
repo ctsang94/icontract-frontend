d9e809298e014afc78af0351557803b831d027cd
Kelvin <ctsang94@gmail.com>
2024-07-02 17:02:07 -0400

    add hover carousel feature

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,17 +5,16 @@ import bathroom from '../../assets/images/bathroom.jpg'
 import bedroom from '../../assets/images/bedroom.jpg'
 import kitchen from '../../assets/images/kitchen.jpg'
 import livingroom from '../../assets/images/livingroom.jpg'
+import HoverCarousel from 'hover-carousel'
 
 function Portfolio() {
+  const images= [basement, bathroom, bedroom, kitchen, livingroom];
+
   return (
     <section className='portfolio'>
         <h2 className='portfolio__title'>Portfolio</h2>
         <article className='portfolio__images'>
-            <img className='portfolio__image' src={basement} />
-            <img className='portfolio__image' src={bathroom} />
-            <img className='portfolio__image' src={bedroom} />
-            <img className='portfolio__image' src={kitchen} />
-            <img className='portfolio__image' src={livingroom} />
+          <HoverCarousel className='portfolio__image' images={images} />
         </article>
 
     </section>
