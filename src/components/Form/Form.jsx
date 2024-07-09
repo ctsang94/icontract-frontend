4b4e940a4834e01adbad3164b3f55264caaab077
Kelvin <ctsang94@gmail.com>
2024-07-09 18:39:52 -0400

    Add frontend form submission

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,21 +1,62 @@
 import React from "react";
 import './Form.scss'
 import { Button } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
 function Form() {
+  const [projectType, setProjectType] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+  const [projectEstimatedBudget, setProjectEstimatedBudget] = useState('');
+  const [projectStartDate, setProjectStartDate] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/projects', {
+        "projectType": projectType,
+        "projectDescription": projectDescription,
+        "estimatedBudget": projectEstimatedBudget,
+        "preferredStartDate": projectStartDate
+      })
+      console.log(response)
+      if (response.status === 201) {
+        alert('Project added successfully!');
+        setProjectType('');
+        setProjectDescription('');
+        setProjectEstimatedBudget('');
+        setProjectStartDate('');
+      } else {
+        alert('Failed to add project');
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      alert('An error occured while adding the project');
+    }
+  }
+
   return (
     <main className='form'>
       <h1 className='form__title'>Start a New Project</h1>
-      <form className='form__container'>
+      <form className='form__container' onSubmit={handleSubmit}>
         <label className='form__label'>Project type</label>
-        <input className='form__input' type='text' placeholder="What's the purpose of this project?"></input>
+        <input className='form__input'
+          value={projectType}
+          onChange={(e) => setProjectType(e.target.value)} type='text' placeholder="What's the purpose of this project?"></input>
         <label className='form__label'>Project description</label>
-        <input className='form__input form__input-desc' type='text' placeholder="What are you looking to accomplish?"></input>
+        <textarea className='form__input form__input-desc'
+          value={projectDescription}
+          onChange={(e) => setProjectDescription(e.target.value)} type='text' placeholder="What are you looking to accomplish?" />
         <label className='form__label'>Estimated budget</label>
-        <input className='form__input' type='text' placeholder="$50,000"></input>
+        <input className='form__input'
+          type="number"
+          value={projectEstimatedBudget}
+          onChange={(e) => setProjectEstimatedBudget(e.target.value)} placeholder="$50,000"></input>
         <label className='form__label'>Preferred start date</label>
-        <input className='form__input' type='text' placeholder="Select date"></input>
-        <Button>Submit</Button>
+        <input className='form__input' type='text'
+          value={projectStartDate}
+          onChange={(e) => setProjectStartDate(e.target.value)} placeholder="Select date"></input>
+        <Button type='submit'>Add Project</Button>
       </form>
     </main>
   );
--- src/components/NewProjects/NewProjects.jsx
+++ src/components/NewProjects/NewProjects.jsx
@@ -10,6 +10,19 @@ import Form from "../Form/Form";
 import { Modal} from "@mui/material";
 import {Box} from "@mui/material";
 
+const style = {
+  position: 'absolute',
+  top: '25%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  height: 600,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 function NewProjects() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -36,7 +49,7 @@ function NewProjects() {
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
       >
-        <Box className="modal-box">
+        <Box className="modal-box" sx={style}>
           <Form />
         </Box>
       </Modal>
