80bc0d6a7ef4a462656a8d5a86876d719cc5b2da
Kelvin <ctsang94@gmail.com>
2024-07-10 17:18:28 -0400

    Add chat to contractor page

--- src/components/Contractor/Contractor.jsx
+++ src/components/Contractor/Contractor.jsx
@@ -3,6 +3,7 @@ import { Button } from '@mui/material'
 import image from '../../assets/images/img.jpg'
 import './Contractor.scss'
 
+
 function Contractor() {
   return (
     <section className='contractor'> 
@@ -10,9 +11,10 @@ function Contractor() {
         <h2 className='contractor__title'>Kelvin's Home Renovation</h2>
         <span className='contractor__review'>4.9(124 reviews)</span>
         <div className='contractor__buttons'>
-            <Button variant='contained'>Contact Me</Button>
-            <Button variant='contained'>Follow</Button>
+            <Button variant='outlined'>Contact Me</Button>
+            <Button variant='outlined'>Follow</Button>
         </div>
+        
     </section>
   )
 }
--- src/pages/Contractorpage.jsx
+++ src/pages/Contractorpage.jsx
@@ -4,6 +4,7 @@ import AboutMe from '../components/AboutMe/AboutMe'
 import Portfolio from '../components/Portfolio/Portfolio'
 import BasicTimeline from '../components/Timeline/Timeline'
 import ContactMe from '../components/ContactMe/ContactMe'
+import Chat from '../components/Chat/Chat.jsx'
 
 function Contractorpage() {
   return (
@@ -13,6 +14,7 @@ function Contractorpage() {
         <Portfolio />
         <BasicTimeline />
         <ContactMe />
+        <Chat />
     </main>
   )
 }
