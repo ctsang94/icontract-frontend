d347d964bf12afa21e21b6651c582306779a62d2
Kelvin <ctsang94@gmail.com>
2024-07-11 10:58:27 -0400

    more styling

--- src/components/ActiveProjects/ActiveProjects.jsx
+++ src/components/ActiveProjects/ActiveProjects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './ActiveProjects.scss';
 import basement from '../../assets/images/basement.jpg'
-import bathroom from '../../assets/images/bathroom.jpg'
+import bathroom from '../../assets/images/homereno1.jpeg'
 import bedroom from '../../assets/images/bedroom.jpg'
 import kitchen from '../../assets/images/kitchen.jpg'
 import livingroom from '../../assets/images/livingroom.jpg'
@@ -27,6 +27,13 @@ function ActiveProjects() {
         </article>
         <article className="active-projects__item">
           <img className="active-projects__image" alt="Project" src={livingroom}/>
+          <div className="active-projects__details">
+            <h3 className="active-projects__details-heading">Bathroom Remodel</h3>
+            <p className="active-projects__details-paragraph">3 quotes 1 new message</p>
+          </div>
+        </article>
+        <article className="active-projects__item">
+          <img className="active-projects__image" alt="Project" src={bathroom}/>
           <div className="active-projects__details">
             <h3 className="active-projects__details-heading">Bathroom Remodel</h3>
             <p className="active-projects__details-paragraph">3 quotes 1 new message</p>
