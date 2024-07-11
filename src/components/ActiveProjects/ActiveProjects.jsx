5e3387e5b2a17b6e61658376b351b2cdc4c15f17
Kelvin <ctsang94@gmail.com>
2024-07-11 15:12:18 -0400

    add style

--- src/components/ActiveProjects/ActiveProjects.jsx
+++ src/components/ActiveProjects/ActiveProjects.jsx
@@ -36,7 +36,7 @@ function ActiveProjects() {
             <p className="active-projects__details-progress"> {project.status}</p>
           </div>
         </article>))}
-        <article className="active-projects__item">
+        {/* <article className="active-projects__item">
           <img className="active-projects__image" alt="Project" src={kitchen} />
           <div className="active-projects__details">
             <h3 className="active-projects__details-heading">Bathroom Remodel</h3>
@@ -56,7 +56,7 @@ function ActiveProjects() {
             <h3 className="active-projects__details-heading">Bathroom Remodel</h3>
             <p className="active-projects__details-paragraph">3 quotes 1 new message</p>
           </div>
-        </article>
+        </article> */}
       </section>
     </main>
   );
